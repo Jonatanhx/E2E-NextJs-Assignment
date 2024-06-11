b964522323f91bfbb1bd7c82c9be7172022041d8
Jonatan Helander <jonatanhelander@hotmail.com>
2024-06-11 10:13:56 +0200

    Batman

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import db from "./db";
+
+async function seedUser() {
+  db.user.upsert({
+    where: { id: 1 },
+    update: {
+      name: "jonatan helander",
+    },
+    create: {
+      name: "jonatan helander",
+      email: "jonatanhelander@hotmail.com",
+    },
+  });
+}
+
+async function main() {
+  await seedUser();
+}
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
