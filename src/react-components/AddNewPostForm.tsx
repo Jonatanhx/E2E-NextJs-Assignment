e515fb34634cc953a0cfc739f156eaa3ed4cc31c
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-06-12 12:32:22 +0200

    All fields added to form

--- src/react-components/AddNewPostForm.tsx
+++ src/react-components/AddNewPostForm.tsx
@@ -45,14 +45,37 @@ export default function AddNewPostForm() {
                   </FormControl>
                   <FormLabel>Image</FormLabel>
                   <FormControl>
-                    <Input placeholder="www.google.com" {...field} />
+                    <Input type="file" placeholder="www.google.com" />
+                  </FormControl>
+                  <FormLabel>Text</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Lorem ipsum..." />
                   </FormControl>
                   <FormDescription></FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
             />
-            <Button type="submit" className="bg-white"></Button>
+            <Button
+              type="submit"
+              className="bg-white text-black border-black border-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 4.5v15m7.5-7.5h-15"
+                />
+              </svg>
+              Post
+            </Button>
           </form>
         </Form>
       </Card>
