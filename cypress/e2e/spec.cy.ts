fb69cd1668b784efedb75d0248afec80004b2d48
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-06-18 18:02:09 +0200

    added additional test and check for undefined session in logged in user

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -17,12 +17,29 @@ describe("Log in with credentials and log out", () => {
 });
 
 describe("Fill out the form and create a post", () => {
-  it("Fills out the form to create a post and verifies that a post is rendered", () => {
+  it("Fills out the form to create a post and verifies that the post is rendered and that the form is emptied for the next post", () => {
     cy.login();
     cy.get('[data-cy="title"]').type("My first post");
-    cy.get('[data-cy="image"]').selectFile("./public/Screenshot_1.png");
+    cy.get('[data-cy="image"]').type(
+      "https://helios-i.mashable.com/imagery/articles/01RhPcOiy9rYKba0BstQt3m/images-1.fill.size_2000x2000.v1611692400.jpg"
+    );
     cy.get('[data-cy="content"]').type("Hello world!");
     cy.get('[data-cy="submit-button"]').click();
     cy.get('[data-cy="post"]').should("have.length", 2);
+
+    cy.get('[data-cy="title"]').should("be.empty");
+    cy.get('[data-cy="image"]').should("be.empty");
+    cy.get('[data-cy="content"]').should("be.empty");
   });
 });
+
+describe("Fill out a form with invalid input, check for error correctly", () => {
+  it("Fills out the form with incorrect values and makes sure the errors are handled", () => {
+    cy.login();
+    cy.get('[data-cy="title"]').type("1");
+    cy.get('[data-cy="submit-button"]').click();
+  });
+});
+
+/* describe("Profile page should only render  ");
+ */
--- src/react-components/LoggedInUser.tsx
+++ src/react-components/LoggedInUser.tsx
@@ -4,22 +4,29 @@ import db from "../../prisma/db";
 
 export default async function LoggedInUser() {
   const session = await auth();
+  if (!session || !session.user || !session.user.email) {
+    return (
+      <div className="flex flex-row items-center">
+        <p className="text-white text-xl pr-5">Please re-log</p>
+      </div>
+    );
+  }
 
   const loggedInUser = await db.user.findUnique({
     where: {
-      email: session!.user!.email!,
+      email: session.user.email,
     },
   });
 
   return (
     <div className="flex flex-row items-center">
-      <p className="text-white text-xl pr-5">Welcome {session!.user?.name}</p>
+      <p className="text-white text-xl pr-5">Welcome {session.user.name}</p>
       <Image
-        src={loggedInUser?.profilePicture!}
+        src={loggedInUser?.profilePicture || "/default-profile-picture.png"}
         alt="Github profile picture"
         width={100}
         height={100}
-      ></Image>
+      />
     </div>
   );
 }
